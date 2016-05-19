210b33b51e0fe291e47358d0cf670a4b27a1b89d
Chad Mills <chad.mills@gmail.com>
2016-05-18 20:27:04 -0400

    failed attempts to add chad's tests

---
+++ symengine/euclidalg.cpp
@@ -0,0 +1,5 @@
+/**
+ *  \file euclidalg.cpp
+ *  NOT PART OF MAIN SYMENGINE BRANCH
+ *	author: Chad Mills
+ **/
---
+++ symengine/euclidalg.h
@@ -0,0 +1,15 @@
+/**
+ *  \file euclidalg.h
+ *  NOT PART OF MAIN SYMENGINE BRANCH
+ *	author: Chad Mills
+ **/
+#ifndef SYMENGINE_EUCLIDALG.H
+#define SYMENGINE_EUCLIDALG.H
+
+#include <symengine/polynomial.h>
+
+ namespace SymEngine {
+ 	
+ }
+
+ #endif
